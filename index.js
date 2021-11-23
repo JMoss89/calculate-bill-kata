f97652c4d51cd7c59818669b95cbe25722b6fc42
July Moss <july.x.moss@gmail.com>
2021-11-23 18:26:01 +0000

    index.js passed all tests

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const calculateBill=()=>{
+const calculateBill=(price, vat, tip)=>{
+    if (typeof price !== "number" || typeof vat !== "number" || typeof tip !== "number"){
+        return "error";
+    }else{
+        let totalToPay="";
+const totalVat=price*(vat/100);
+const totalTip=price*(tip/100);
+const totalNumber=price+totalVat+totalTip;
 
+totalToPay="£"+totalNumber;
+return totalToPay;
+    }
 }
 
 module.exports=calculateBill;
